17f6de43144d57467d76381d85a4475fe95aa034
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-09-22 16:28:45 +0900

    Update argocd_rolebinding.sh

--- argocd_rolebinding.sh
+++ argocd_rolebinding.sh
@@ -4,7 +4,7 @@ while [ $x -le 35 ]
 do
   namespace="edu${x}"
   echo $namespace
-  k_exec=`kubectl create clusterrolebinding argocd-rolebinding${x} --clusterrole=node-view-role --user=${namespace}-admin`
+  k_exec=`kubectl create clusterrolebinding argocd-rolebinding${x} --clusterrole=argocd-role --user=${namespace}-admin`
   echo $k_exec
   sleep 1
   x=$(( $x + 1 ))
